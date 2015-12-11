607e0f6d456ecb3a886f39fa4efb6d395c650b36
Jason <quizathaderat@yahoo.com>
2015-12-10 19:49:36 -0800

    Initial commit

---
+++ client/party-details/party-details.ts
@@ -0,0 +1,21 @@
+/// <reference path='../../typings/angular2-meteor.d.ts' />
+
+import {Component, View} from 'angular2/angular2';
+import {RouteParams} from 'angular2/router';
+import {Parties} from 'collections/parties';
+
+@Component({
+  selector: 'party-details'
+})
+
+@View({
+  templateUrl: '/client/party-details/party-details.html'
+})
+
+export class PartyDetails {
+  party: Object;
+    constructor(params: RouteParams) {
+        var partyId = params.get('partyId');
+        this.party = Parties.findOne(partyId);
+    }
+}
